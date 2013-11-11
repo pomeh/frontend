a84fabedae8841677ebcc2fd1bca5afd68188a76
Darren Hurley <darren.hurley@guardian.co.uk>
2013-11-11 14:47:26 +0000

    fixed that map inline issue

--- facia/app/views/support/package.scala
+++ facia/app/views/support/package.scala
@@ -8,14 +8,14 @@ object FindStyle {
    * Mapping of collection 'type' to Style - in general, this should suffice
    */
   val generalStyles: Map[String, Container] = Map(
-    "news-stories" -> NewsContainer(),
-    "comment-stories" -> CommentContainer(),
-    "feature-stories" -> FeaturesContainer(),
-    "section-news-stories" -> SectionContainer(),
-    "section-comment-stories" -> SectionContainer(tone = "comment"),
-    "section-feature-stories" -> SectionContainer(tone = "feature"),
-    "regular-stories" -> SectionContainer(),
-    "special-story" -> NewsContainer()
+    ("news-stories", NewsContainer()),
+    ("comment-stories", CommentContainer()),
+    ("feature-stories", FeaturesContainer()),
+    ("section-news-stories", SectionContainer()),
+    ("section-comment-stories", SectionContainer(tone = "comment")),
+    ("section-feature-stories", SectionContainer(tone = "feature")),
+    ("regular-stories", SectionContainer()),
+    ("special-story", NewsContainer())
   )
 
   /**
